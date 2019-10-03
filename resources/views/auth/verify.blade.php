2d3ad87128c103d396223abf71066bd9054dd134
[TakayukiNJ] <[nj.takayuki@gmail.com]>
2019-10-04 01:44:36 +0900

    日本語化できた

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -13,12 +13,11 @@
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
-                    [ここも英語のまま]
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Before proceeding, please check your email for a verification link.') }}<br>
+                    {{ __('If you did not receive the email') }}
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-		                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+		                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
 	                </form>
                 </div>
             </div>
